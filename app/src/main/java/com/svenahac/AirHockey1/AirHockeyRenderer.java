8ad2a8c0b08d2191631fd7eb692bc720cc69a26f
svenahac <ahacsh@gmail.com>
2023-03-06 11:06:03 +0100

    Completed Tutorial 3

--- app/src/main/java/com/svenahac/AirHockey1/AirHockeyRenderer.java
+++ app/src/main/java/com/svenahac/AirHockey1/AirHockeyRenderer.java
@@ -23,20 +23,33 @@ public class AirHockeyRenderer implements Renderer {
     private final Context context;
     private static final int BYTES_PER_FLOAT = 4;
     private final FloatBuffer vertexData;
-    private int uColorLocation,aPositionLocation;
+    private int aPositionLocation, uSizeLocation, aColorLocation;
+    private static final int COLOR_COMPONENT_COUNT = 3;
     private static final int POSITION_COMPONENT_COUNT = 2;
+    private static final int STRIDE = (POSITION_COMPONENT_COUNT + COLOR_COMPONENT_COUNT) * BYTES_PER_FLOAT;
+
     public AirHockeyRenderer(Context context) {
         this.context = context;
 
         float[] tableVerticesWithTriangles = {
-                // Triangle 1
-                -0.5f, -0.5f, 0.5f, 0.5f, -0.5f, 0.5f,
-                // Triangle 2
-                -0.5f, -0.5f, 0.5f, -0.5f, 0.5f, 0.5f,
+                //Order: X,Y,R,G,B
+                // Triangle Fan
+                0f,0f,1f,1f,1f,
+                -0.5f, -0.5f, 0.7f, 0.7f, 0.7f,
+                0.5f, -0.5f, 0.7f, 0.7f, 0.7f,
+                0.5f, 0.5f, 0.7f, 0.7f, 0.7f,
+                -0.5f, 0.5f, 0.7f, 0.7f, 0.7f,
+                -0.5f, -0.5f, 0.7f, 0.7f, 0.7f,
                 // Line 1
-                -0.5f, 0f, 0.5f, 0f,
-                // Mallets
-                0f, -0.25f, 0f, 0.25f
+                -0.5f, 0f, 1f, 0f, 0f, 0.5f, 0f, 1f, 0f, 0f,
+                // Mallet Blue
+                0f, -0.25f, 0f,0f,1f,
+                // Mallet Red
+                0f, 0.25f, 1f,0f,0f,
+                // Puck
+                0f, 0f, 1f,0f,0f
+
+
         };
 
         vertexData = ByteBuffer.allocateDirect(
@@ -48,7 +61,7 @@ public class AirHockeyRenderer implements Renderer {
 
     @Override
     public void onSurfaceCreated(GL10 glUnused, EGLConfig config) {
-        glClearColor(1.0f, 0.0f, 0.0f, 0.0f);
+        glClearColor(0f, 0f, 0f, 1f);
         String vertexShaderSource = TextResourceReader.readTextFileFromResource(context, R.raw.simple_vertex_shader);
         String fragmentShaderSource = TextResourceReader.readTextFileFromResource(context, R.raw.simple_fragment_shader);
         int vertexShader = ShaderHelper.compileVertexShader(vertexShaderSource);
@@ -58,12 +71,17 @@ public class AirHockeyRenderer implements Renderer {
             ShaderHelper.validateProgram(program);
         }
         glUseProgram(program);
-        uColorLocation = glGetUniformLocation(program, "u_Color");
+        aColorLocation = glGetAttribLocation(program, "a_Color");
         aPositionLocation = glGetAttribLocation(program, "a_Position");
+        uSizeLocation = glGetUniformLocation(program, "u_Size");
 
         vertexData.position(0);
-        glVertexAttribPointer(aPositionLocation, POSITION_COMPONENT_COUNT, GL_FLOAT, false, 0, vertexData);
+        glVertexAttribPointer(aPositionLocation, POSITION_COMPONENT_COUNT, GL_FLOAT, false, STRIDE, vertexData);
         glEnableVertexAttribArray(aPositionLocation);
+
+        vertexData.position(POSITION_COMPONENT_COUNT);
+        glVertexAttribPointer(aColorLocation, COLOR_COMPONENT_COUNT, GL_FLOAT, false, STRIDE, vertexData);
+        glEnableVertexAttribArray(aColorLocation);
     }
     @Override
     public void onSurfaceChanged(GL10 glUnused, int width, int height){
@@ -73,16 +91,19 @@ public class AirHockeyRenderer implements Renderer {
     @Override
     public void onDrawFrame(GL10 glUnused) {
         glClear(GL_COLOR_BUFFER_BIT);
-        glUniform4f(uColorLocation, 1.0f, 1.0f, 1.0f, 1.0f);
-        glDrawArrays(GL_TRIANGLES, 0, 6);
+        glDrawArrays(GL_TRIANGLE_FAN, 0, 6);
 
-        glUniform4f(uColorLocation, 1.0f, 0.0f, 0.0f, 1.0f);
+
         glDrawArrays(GL_LINES, 6, 2);
 
-        glUniform4f(uColorLocation, 0.0f, 0.0f, 1.0f, 1.0f);
+        glUniform1f(uSizeLocation, 50f);
         glDrawArrays(GL_POINTS, 8, 1);
 
-        glUniform4f(uColorLocation, 1.0f, 0.0f, 0.0f, 1.0f);
         glDrawArrays(GL_POINTS, 9, 1);
+
+        glUniform1f(uSizeLocation, 10f);
+        glDrawArrays(GL_POINTS, 10, 1);
+
+
     }
 }
--- app/src/main/res/raw/simple_vertex_shader.glsl
+++ app/src/main/res/raw/simple_vertex_shader.glsl
@@ -1,6 +1,9 @@
 attribute vec4 a_Position;
-
+attribute vec4 a_Color;
+uniform float u_Size;
+varying vec4 v_Color;
 void main() {
-    gl_PointSize = 10.0;
+    v_Color = a_Color;
+    gl_PointSize = u_Size;
     gl_Position = a_Position;
 }
